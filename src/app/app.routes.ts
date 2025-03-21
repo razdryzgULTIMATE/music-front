972aedcc717a35f2fdd1242fc320a864bd68e70e
razdryzgULTIMATE <alphanomore2222@gmail.com>
2025-03-21 15:22:03 +0300

    init commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {SignInComponent} from './components/sign-in/sign-in.component';
+import {SignUpComponent} from './components/sign-up/sign-up.component';
+import {UserComponent} from './components/user/user.component';
+import {AdminComponent} from './components/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'sign-in', component: SignInComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'cabinet/:username', component: UserComponent},
+  {path: 'admin', component: AdminComponent},
+
+];
